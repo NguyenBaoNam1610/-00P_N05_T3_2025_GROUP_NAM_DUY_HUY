a8dba9fd84e9fb1b4a5b62854d9c5ead78a47d89
huy <Huyhomhinh21072006@gmail.com>
2025-07-12 10:15:07 +0000

    update

--- src/Booking.java
+++ src/Booking.java
@@ -1,8 +1,48 @@
 
 
-public class Booking {
-    public int C_id;
-    public int R_id;
-    public String checkin;
-    public String checkout;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookingService {
+    private List<Booking> bookings = new ArrayList<>();
+
+    public void createBooking(Booking booking) {
+        bookings.add(booking);
+        System.out.println("Booking created: " + booking);
+    }
+
+    public Booking getBookingByCustomerId(int c_id) {
+        for (Booking b : bookings) {
+            if (b.C_id == c_id) {
+                return b;
+            }
+        }
+        return null;
+    }
+
+    public List<Booking> getAllBookings() {
+        return bookings;
+    }
+
+    public boolean updateBooking(int c_id, Booking newBookingData) {
+        for (int i = 0; i < bookings.size(); i++) {
+            if (bookings.get(i).C_id == c_id) {
+                bookings.set(i, newBookingData);
+                System.out.println("Booking updated: " + newBookingData);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean deleteBooking(int c_id) {
+        for (int i = 0; i < bookings.size(); i++) {
+            if (bookings.get(i).C_id == c_id) {
+                bookings.remove(i);
+                System.out.println("Booking deleted for C_id=" + c_id);
+                return true;
+            }
+        }
+        return false;
+    }
 }
