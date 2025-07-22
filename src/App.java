04293171988c5035305419d28f505c4d62ee7417
duyphamdevx <duy.phmthe@gmail.com>
2025-07-22 10:28:19 +0000

    Implement bookRoomForCustomer in Main class

--- src/App.java
+++ src/App.java
@@ -1,11 +1,7 @@
 
 public class App {
     public static void main(String[] args) {
-        //TestBooking.test();
-        //TestCustomer.test();
-        TestRoom.test();
-        //Leaf.test();
-        //Person.test();
+        TestMain.test();
     }
     
 }
